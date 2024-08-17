69dd623c123fbb67020e2ac792f7cd1417190b30
Nisha <nkan0018@student.monash.edu>
2024-08-17 16:04:35 +0800

    Update UI with form

--- frontend/entry-functions/mint_nft.ts
+++ frontend/entry-functions/mint_nft.ts
@@ -3,15 +3,17 @@ import { InputTransactionData } from "@aptos-labs/wallet-adapter-react";
 export type MintNftArguments = {
   collectionId: string;
   amount: number;
+  prescriptionData: string;
 };
 
 export const mintNFT = (args: MintNftArguments): InputTransactionData => {
-  const { collectionId, amount } = args;
+  const { collectionId, amount, prescriptionData } = args;
   return {
     data: {
       function: `${import.meta.env.VITE_MODULE_ADDRESS}::launchpad::mint_nft`,
       typeArguments: [],
-      functionArguments: [collectionId, amount],
+      functionArguments: [collectionId, amount, prescriptionData],
+      
     },
   };
 };
